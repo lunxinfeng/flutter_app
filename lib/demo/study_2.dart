3bc5b2851f8d111631fe0a5db4162e06a1ac47dd
lunxinfeng <892452936@qq.com>
2019-03-08 10:23:43 +0800

    flutter study

--- lib/demo/study_2.dart
+++ lib/demo/study_2.dart
@@ -6,15 +6,27 @@ void main() {
 }
 
 const String PAGE_2 = "/page2";
+const String PAGE_3 = "/page3";
 
-final Map<String, WidgetBuilder> _routes = {
-  PAGE_2: (_) => Page2(),
+final Map<String, Function> _routes = {
+  PAGE_2: (context, {arguments}) => Page2(arguments: arguments),
+  PAGE_3: (context, {arguments}) => Page3(arguments: arguments),
 };
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(routes: _routes, home: Page1());
+    return MaterialApp(
+//      routes: _routes,
+      home: Page1(),
+      onGenerateRoute: (routeSetting) {
+        Function _routeGenerate = _routes[routeSetting.name];
+        if (_routeGenerate != null)
+          return MaterialPageRoute(
+              builder: (context) =>
+                  _routeGenerate(context, arguments: routeSetting.arguments));
+      },
+    );
   }
 }
 
@@ -40,6 +52,57 @@ class Page1 extends StatelessWidget {
               },
               child: Text("push"),
             ),
+            RaisedButton(
+              onPressed: () {
+                print('Page1 pushNamed Page2');
+                Navigator.pushNamed(context, PAGE_2,
+                    arguments: {"name": "lili"}).then((value) {
+                  print('Page1 pushNamed result: $value');
+                });
+              },
+              child: Text("pushNamed"),
+            ),
+            RaisedButton(
+              onPressed: () {
+                print('Page1 push Page2 with anim');
+                Navigator.push(
+                    context,
+                    PageRouteBuilder(
+                        pageBuilder: (context, animation, secondaryAnimation) {
+                          return Page2();
+                        },
+                        transitionsBuilder:
+                            (context, animation, secondaryAnimation, child) {
+                          return FadeTransition(
+                            opacity: animation,
+                            child: RotationTransition(
+                              turns: Tween(begin: 0.0, end: 1.0)
+                                  .animate(animation),
+                              child: child,
+                            ),
+                          );
+                        },
+                        transitionDuration: Duration(milliseconds: 500)));
+              },
+              child: Text("push with anim"),
+            ),
+            RaisedButton(
+              onPressed: () {
+                print('Page1 showDialog');
+                showGeneralDialog(
+                    context: context,
+                    barrierDismissible: true,
+                    barrierLabel: "Dismiss",
+                    barrierColor: Color.fromRGBO(255, 0, 0, 0.5),
+                    transitionDuration: Duration(milliseconds: 300),
+                    pageBuilder: (context, animation, secondaryAnimation) {
+                      return AlertDialog(
+                        title: Text("标题"),
+                      );
+                    });
+              },
+              child: Text("showDialog"),
+            ),
             Text("Page1")
           ],
         ),
@@ -49,9 +112,14 @@ class Page1 extends StatelessWidget {
 }
 
 class Page2 extends StatelessWidget {
+  Map<String, Object> arguments;
+
+  Page2({this.arguments});
+
   @override
   Widget build(BuildContext context) {
     print('Page2 build');
+    print('arguments:$arguments');
     return Scaffold(
       backgroundColor: Colors.green,
       body: Container(
@@ -81,16 +149,13 @@ class Page2 extends StatelessWidget {
             ),
             RaisedButton(
               onPressed: () {
-                print(
-                    'Page2 pushAndRemoveUntil Page3');
+                print('Page2 pushAndRemoveUntil Page3');
                 Navigator.pushAndRemoveUntil(
-                        context,
-                        MaterialPageRoute(builder: (context) => Page3()),
-                        (route) {
-                          print('route:$route');
-                          return route.settings.name == "/";
-                        })
-                    .then((value) {
+                    context, MaterialPageRoute(builder: (context) => Page3()),
+                    (route) {
+                  print('route:$route');
+                  return route.settings.name == "/";
+                }).then((value) {
                   print('Page2 pushAndRemoveUntil result: $value');
                 });
               },
@@ -103,7 +168,7 @@ class Page2 extends StatelessWidget {
               },
               child: Text("返回"),
             ),
-            Text("Page2")
+            Text("Page2 arguments:$arguments")
           ],
         ),
       ),
@@ -112,6 +177,10 @@ class Page2 extends StatelessWidget {
 }
 
 class Page3 extends StatelessWidget {
+  Map<String, Object> arguments;
+
+  Page3({this.arguments});
+
   @override
   Widget build(BuildContext context) {
     print('Page3 build');
